4e226b86c66ba112654316afc6e9bc42c7259bf0
ThatNerdSquared <72814106+ThatNerdSquared@users.noreply.github.com>
2024-11-21 18:03:48 -0800

    feat: clean input data first

--- pipeline.py
+++ pipeline.py
@@ -1,7 +1,7 @@
 from pymol import cmd
 
 # load the raw PDB
-cmd.load("data/1KFs_raw.pdb")
+cmd.load("data/1KFS_wt.pdb")
 
 # activate pymol wizard
 cmd.wizard("mutagenesis")
@@ -12,33 +12,4 @@ cmd.get_wizard().apply()
 cmd.get_wizard().do_select("chain A and residue 357")
 cmd.get_wizard().apply()
 cmd.set_wizard("done")
-cmd.save("data/1KFs_mt.pdb", pdb)
-
-"""
-OPTIMIZER_AA_SEQ = Seq.Seq("MGSSHHHHHHSSGLVPRGSHMASMTGGQQMGRGSEFV")
-
-# read in raw data
-records = list(SeqIO.parse("data/1KFs.fasta", "fasta"))
-raw_oligo_seq = records[0].seq
-raw_wt_seq = records[1].seq
-
-# add optimizer sequence
-wt_seq = OPTIMIZER_AA_SEQ + raw_wt_seq[1:]
-
-# mutate WT sequence to match Klenow Exo– (MT)
-# at position 68: D->A
-# at position 70: E->A
-mt_seq = Seq.MutableSeq(wt_seq)
-assert(mt_seq[67] == "D")
-assert(mt_seq[69] == "E")
-mt_seq[67] = "A"
-mt_seq[69] = "A"
-mt_seq = Seq.Seq(mt_seq)
-
-# write back out to FASTA files
-wt_seq_record = SeqRecord.SeqRecord(wt_seq, id="Klenow Fragment (WT)")
-mt_seq_record = SeqRecord.SeqRecord(mt_seq, id="Klenow Exo- (MT)")
-
-SeqIO.write(wt_seq_record, "data/1KFs_wt.fasta", "fasta")
-SeqIO.write(mt_seq_record, "data/1KFs_mt.fasta", "fasta")
-"""
+cmd.save("data/1KFS_mt.pdb", pdb)
--- setup.sh
+++ setup.sh
@@ -2,5 +2,9 @@
 
 # download PDB 1KFS (Klenow Fragment WT)
 mkdir -p data
-curl -O "https://www.rcsb.org/fasta/entry/1KFS"
-mv 1KFs data/1KFs_raw.fasta
+curl -O "https://files.rcsb.org/view/1KFS.pdb"
+mv 1KFS.pdb data/1KFS_raw.pdb
+
+# split oligo from protein, clean out ions/waters
+pdb_selchain -A data/1KFs_raw.pdb | pdb_delhetatm > data/1KFS_wt.pdb
+pdb_selchain -B data/1KFs_raw.pdb | pdb_delhetatm > data/oligo.pdb
